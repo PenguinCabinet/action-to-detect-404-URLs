d34f01551d49fc8ebdcdbef8c86abb19b0065d0f
PenguinCabinet <kage2.kage22@gmail.com>
2026-01-09 22:39:34 +0900

    Fix

--- test_main.py
+++ test_main.py
@@ -1,7 +1,10 @@
 from main import *
 import unittest
 
+unittest.util._MAX_LENGTH = 10**7
+
 class TestMain(unittest.TestCase):
+    maxDiff = None
     def test_search_urls(self):
         self.assertEqual(
             search_urls("https://example.com example.com https://example.com"),
@@ -22,8 +25,11 @@ class TestMain(unittest.TestCase):
             ]
         )
         self.assertEqual(
-            search_and_filter_urls_directory("./test","allow","2..",True,1),
-            [
+            sorted(
+                search_and_filter_urls_directory("./test","allow","2..",True,1),
+                key=lambda v:v["url"]
+            ),
+            sorted([
                 {
                     'status_code': 404,
                     'url': 'http://example.com/404_TEST_TEST'
@@ -32,7 +38,19 @@ class TestMain(unittest.TestCase):
                     'status_code': -1,
                     'url': 'http://example.example'
                 },
-            ]
+            ],key=lambda v:v["url"])
+        )
+        self.assertEqual(
+            sorted(
+                search_and_filter_urls_directory("./test","deny","4..",False,1),
+                key=lambda v:v["url"]
+            ),
+            sorted([
+                {
+                    'status_code': 404,
+                    'url': 'http://example.com/404_TEST_TEST'
+                },
+            ],key=lambda v:v["url"])
         )
 
 if __name__ == '__main__':
